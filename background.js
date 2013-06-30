aa0684607db2008e83bbf42999a87b04d312d9cd
Hiroki Honda <cside.story@gmail.com>
2013-06-30 22:01:56 +0900

    switches when focused window is changed

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 (function() {
+    var p = function() {
+        console.log(JSON.stringify(Array.prototype.slice.call(arguments, 0, arguments.length)));
+    }
     var defaults = {
         ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
         width: 640,
@@ -10,25 +13,33 @@
         }
     });
 
-    var CurrentWidth;
+    var CurrentWidth = localStorage.width;
 
     var onMessageHandler = function (width) {
         if (! width) return;
         CurrentWidth = width;
     };
+    var onFocusHandler = function (tabId) {
+        chrome.tabs.sendMessage(tabId, {}, onMessageHandler);
+    };
     chrome.tabs.onActivated.addListener(function (info) {
-        chrome.tabs.sendMessage(info.tabId, {}, onMessageHandler);
+        onFocusHandler(info.tabId);
+    });
+    chrome.windows.onFocusChanged.addListener(function () {
+        chrome.tabs.getSelected(function (tab) {
+            onFocusHandler(tab.id);
+        });
     });
     chrome.runtime.onMessage.addListener(onMessageHandler);
 
     chrome.webRequest.onBeforeSendHeaders.addListener(
         function (details) {
             var threshold = localStorage.width;
-            var ua        = localStorage.ua;
+            // p({ current: CurrentWidth, threshold: threshold });
             if (CurrentWidth <= threshold) {
                 for (var i = 0, l = details.requestHeaders.length; i < l; ++i) {
                     if (details.requestHeaders[i].name === 'User-Agent') {
-                        details.requestHeaders[i].value = ua;
+                        details.requestHeaders[i].value = localStorage.ua;
                         break;
                     }
                 }
